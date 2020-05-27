6394f42561236309551cdab27aa6412d12d9691f
Alan <AlanWhereAreYou@gmail.com>
2020-05-27 15:26:16 -0400

    Updated as-built and working

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,8 @@
 // Date and time functions using a DS3231 RTC connected via I2C and Wire lib
+// rtc.adjust(DateTime(2014, 1, 21, 3, 0, 0));
 #include "RTClib.h"
 RTC_DS3231 rtc;
-char daysOfTheWeek[7][12] = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
+// char daysOfTheWeek[7][12] = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
 
 #include "TM1637Display.h"
 // Define the connections pins:
@@ -9,18 +10,24 @@ char daysOfTheWeek[7][12] = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursd
 #define DIO 11
 // Create display object of type TM1637Display:
 TM1637Display display = TM1637Display(CLK, DIO);
-// Create array that turns all segments on:
-const uint8_t data[] = {0xff, 0xff, 0xff, 0xff};
-// Create array that turns all segments off:
-const uint8_t blank[] = {0x00, 0x00, 0x00, 0x00};
+const uint8_t data[] = {0xff, 0xff, 0xff, 0xff}; // All Segments On
+const uint8_t blank[] = {0x00, 0x00, 0x00, 0x00}; // All Segments Off
 // You can set the individual segments per digit to spell words or create other symbols:
-const uint8_t done[] = {
+/* const uint8_t done[] = {
   SEG_B | SEG_C | SEG_D | SEG_E | SEG_G,           // d
   SEG_A | SEG_B | SEG_C | SEG_D | SEG_E | SEG_F,   // O
   SEG_C | SEG_E | SEG_G,                           // n
   SEG_A | SEG_D | SEG_E | SEG_F | SEG_G            // E
-};
+}; */
+const int display_BrightnessMax = 7;
+const int display_BrightnessMin = 1;
 
+#include "Eeprom24C32_64.h"
+#define EEPROM_ADDRESS  0x57
+static Eeprom24C32_64 eeprom(EEPROM_ADDRESS);
+const word addr_Brightness = 0;
+
+// Pin Defs
 #define Pin_Hour_Up 3
 #define Pin_Hour_Down 2
 #define Pin_10Min_Up 5
@@ -30,7 +37,10 @@ const uint8_t done[] = {
 #define Pin_Brightness 8
 #define Pin_Spare 9
 
-#define DEBUG
+// Global Config
+uint8_t cfg_Brightness = 7;
+
+
 
 struct button
 {
@@ -66,52 +76,66 @@ void ProcessButton(button *b, bool NewValue)
     }    
 }
 
-// General idea
-// Check buttons in an interrupt
-// Main loop will see if an unprocessed button press has occured
-// If so, process the button press
-// If time change: Add/subtract time and save to RTC zeroing the seconds.
-// On startup read time from RTC
-// Read time from RTC every main loop
+int get12h_FormatHours(int hours_24h_format)
+{
+  int hours_12h_format;
+  if (hours_24h_format > 12) 
+  {
+      hours_12h_format = hours_24h_format - 12; // Subtract 12 if the hours are above 12.
+  } 
+  else
+  {
+      hours_12h_format = hours_24h_format;
+  }
+  
+  if(hours_12h_format == 0) {hours_12h_format = 12;}
+  return hours_12h_format;
+}
 
-void ShowTime(DateTime value)
+void ShowTime(DateTime datevalue)
 {
   static uint8_t TimeData[4]; 
 
-    // Decode into digits
-    int h10;
-    int h1;
-    int m10;
-    int m1;
-    int seconds; // If we are in an even second
+  // Decode into digits
+  int hours_12h_format;
+  int h10;
+  int h1;
+  int m10;
+  int m1;
+  int seconds; // If we are in an even second
 
-    h10 = value.hour() / 10;
-    h1 = value.hour() - h10*10;
-    m10 = value.minute() / 10;
-    m1 = value.minute() - m10*10;
-    seconds = value.second();
+  hours_12h_format = get12h_FormatHours(datevalue.hour());
 
-    TimeData[0] = display.encodeDigit(h10);
-    TimeData[1] = display.encodeDigit(h1) | (SEG_DP * (seconds/2) ); // Show the : every even second
-    TimeData[2] = display.encodeDigit(m10);
-    TimeData[3] = display.encodeDigit(m1);
+  h10 = hours_12h_format / 10;
+  h1 = hours_12h_format - h10*10;
+  m10 = datevalue.minute() / 10;
+  m1 = datevalue.minute() - m10*10;
+  seconds = datevalue.second();
 
+  // We don't want t leading zero (e.g. 1:23 not 01:23)
+  if(h10 !=0)
+    { TimeData[0] = display.encodeDigit(h10);} // Not a zero, leave the digits.
+  else
+    { TimeData[0] = 0;} // Blank the digit.
 
-    display.setSegments(TimeData);
+  TimeData[1] = display.encodeDigit(h1) | (SEG_DP * (seconds/2) ); // Show the : every even second
+  TimeData[2] = display.encodeDigit(m10);
+  TimeData[3] = display.encodeDigit(m1);
+
+  display.setSegments(TimeData);
 }
 
-void setup () {
-    Serial.begin(9600);
-    Serial.println("Starting");
-    
+void startUp_Display()
+{
     // Clear the display:
-    display.setBrightness(0x0f);
-    Serial.println("All segments");
+    display.setBrightness(cfg_Brightness);
     display.setSegments(data);
     delay(1000);
     display.clear();
-    display.showNumberDec(1);
+}
 
+void setupPins()
+{
     pinMode(Pin_Hour_Up, INPUT_PULLUP);
     pinMode(Pin_Hour_Down, INPUT_PULLUP);
     pinMode(Pin_10Min_Up, INPUT_PULLUP);
@@ -120,29 +144,114 @@ void setup () {
     pinMode(Pin_1Min_Down, INPUT_PULLUP);
     pinMode(Pin_Brightness, INPUT_PULLUP);
     pinMode(Pin_Spare, INPUT_PULLUP);
+}
 
-    display.showNumberDec(2);
+void startUp_RTC()
+{
     if (! rtc.begin()) {
       Serial.println("Couldn't find RTC");
       while (1);
     }
-
-    display.showNumberDec(3);
     if (rtc.lostPower()) {
       Serial.println("RTC lost power, lets set the time!");
       // If the RTC have lost power it will sets the RTC to the date & time this sketch was compiled in the following line
       rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
-      // This line sets the RTC with an explicit date & time, for example to set
-      // January 21, 2014 at 3am you would call:
-      // rtc.adjust(DateTime(2014, 1, 21, 3, 0, 0));
     }
+}
 
-    // If you need to set the time of the uncomment line 34 or 37
-    // following line sets the RTC to the date & time this sketch was compiled
-    // rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
-    // This line sets the RTC with an explicit date & time, for example to set
-    // January 21, 2014 at 3am you would call:
-    // rtc.adjust(DateTime(2014, 1, 21, 3, 0, 0));
+void startUp_EEPROM()
+{
+  eeprom.initialize();
+}
+
+void load_EEPROM_Settings()
+{    
+  cfg_Brightness = eeprom.readByte(addr_Brightness);
+  Serial.print("Raw Brightness Value from EEPROM: ");
+  Serial.println(cfg_Brightness);
+  cfg_Brightness = constrain(cfg_Brightness,1,7);
+}
+
+void storeBrightness(int val)
+{
+    Serial.print ("Storing Brightness Value: ");
+    Serial.println (val);
+    eeprom.writeByte(addr_Brightness,val);
+}
+
+void setup () {
+  Serial.begin(9600);
+  Serial.println("Starting");
+  
+  setupPins();
+
+  startUp_EEPROM();
+  load_EEPROM_Settings();
+
+  startUp_Display();
+  startUp_RTC();   
+}
+
+void Add1Hr()
+{
+  rtc.adjust(rtc.now()+TimeSpan(0, 1, 0, 0));
+}
+
+void Sub1Hr()
+{
+  rtc.adjust(rtc.now()+TimeSpan(0, -1, 0, 0));
+}
+
+void Add10Min()
+{
+  DateTime datevalue = rtc.now();
+  int mins = datevalue.minute();
+
+  mins = mins +10;
+  if (mins > 60) {mins = mins - 60;} // Wrap Minutes
+  Serial.println(mins);
+
+  rtc.adjust(DateTime(datevalue.year(),datevalue.month(), datevalue.day(), datevalue.hour(), mins, datevalue.second()));
+}
+
+void Sub10Min()
+{
+  DateTime datevalue = rtc.now();
+  int mins = datevalue.minute();
+
+  mins = mins +10;
+  if (mins > 60) {mins = mins - 60;} // Wrap Minutes
+  Serial.println(mins);
+
+  rtc.adjust(DateTime(datevalue.year(),datevalue.month(), datevalue.day(), datevalue.hour(), mins, datevalue.second()));
+}
+
+void Add1Min()
+{
+  DateTime datevalue = rtc.now();
+  int mins = datevalue.minute();
+  int m10 = datevalue.minute() / 10;
+  int m1 = datevalue.minute() - m10*10;
+
+  m1++;
+  if(m1 > 9) {m1 = 0;}
+  mins = m10 * 10 + m1;
+
+  rtc.adjust(DateTime(datevalue.year(),datevalue.month(), datevalue.day(), datevalue.hour(), mins, 0)); // Adusting the minutes also sets seconds to 0.
+}
+
+void Sub1Min()
+{
+  DateTime datevalue = rtc.now();
+  int mins = datevalue.minute();
+  int m10 = datevalue.minute() / 10;
+  int m1 = datevalue.minute() - m10*10;
+
+  m1--;
+  if(m1 < 0) {m1 = 9;}
+  mins = m10 * 10 + m1;
+
+  rtc.adjust(DateTime(datevalue.year(),datevalue.month(), datevalue.day(), datevalue.hour(), mins, 0)); // Adusting the minutes also sets seconds to 0.
 }
 
 void loop () 
@@ -151,6 +260,7 @@ void loop ()
   now = rtc.now();
 
   ShowTime(now);
+  display.setBrightness(cfg_Brightness);
 
   static button button_Hour_Up;
   ProcessButton(&button_Hour_Up,digitalRead(Pin_Hour_Up));
@@ -158,11 +268,7 @@ void loop ()
   {
     Serial.println("Hour Up Falling Edge");
     button_Hour_Up.falling_edge_latch=false;
-  }
-  if(button_Hour_Up.rising_edge_latch)
-  {
-    //Serial.println("Hour Up Rising Edge");
-    button_Hour_Up.rising_edge_latch=false;
+    Add1Hr();
   }
  
   static button button_Hour_Down;
@@ -171,11 +277,7 @@ void loop ()
   {
     Serial.println("Hour Down Falling Edge");
     button_Hour_Down.falling_edge_latch=false;
-  }
-  if(button_Hour_Down.rising_edge_latch)
-  {
-    //Serial.println("Hour Down Rising Edge");
-    button_Hour_Down.rising_edge_latch=false;
+    Sub1Hr();
   }
 
   static button button_10Min_Up;
@@ -184,11 +286,7 @@ void loop ()
   {
     Serial.println("10Min Up Falling Edge");
     button_10Min_Up.falling_edge_latch=false;
-  }
-  if(button_10Min_Up.rising_edge_latch)
-  {
-    //Serial.println("10Min Up Rising Edge");
-    button_10Min_Up.rising_edge_latch=false;
+    Add10Min();
   }
   
   static button button_10Min_Down;
@@ -197,11 +295,7 @@ void loop ()
   {
     Serial.println("10Min Down Falling Edge");
     button_10Min_Down.falling_edge_latch=false;
-  }
-  if(button_10Min_Down.rising_edge_latch)
-  {
-    //Serial.println("10Min Down Rising Edge");
-    button_10Min_Down.rising_edge_latch=false;
+    Sub10Min();
   }
   
   static button button_1Min_Up;
@@ -210,11 +304,7 @@ void loop ()
   {
     Serial.println("1Min Up Falling Edge");
     button_1Min_Up.falling_edge_latch=false;
-  }
-  if(button_1Min_Up.rising_edge_latch)
-  {
-    //Serial.println("1Min Up Rising Edge");
-    button_1Min_Up.rising_edge_latch=false;
+    Add1Min();
   }
   
   static button button_1Min_Down;
@@ -223,11 +313,7 @@ void loop ()
   {
     Serial.println("1Min Down Falling Edge");
     button_1Min_Down.falling_edge_latch=false;
-  }
-  if(button_1Min_Down.rising_edge_latch)
-  {
-    //Serial.println("1Min Down Rising Edge");
-    button_1Min_Down.rising_edge_latch=false;
+    Sub1Min();
   }
   
   static button button_Brightness;
@@ -236,15 +322,12 @@ void loop ()
   {
     Serial.println("Brightness Falling Edge");
     button_Brightness.falling_edge_latch=false;
-  }
-  if(button_Brightness.rising_edge_latch)
-  {
-    //Serial.println("Brightness Rising Edge");
-    button_Brightness.rising_edge_latch=false;
+    cfg_Brightness++;
+    if (cfg_Brightness > display_BrightnessMax) {cfg_Brightness = display_BrightnessMin;} // Wrap brightness from max back to min.
+    storeBrightness(cfg_Brightness); // Persist to the EEPROM.
   }
   
-
-    delay(100);
+    delay(50);
 }
 
 
